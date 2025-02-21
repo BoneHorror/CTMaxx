7839516150a46ecbd949cd8102cfb7089324cec7
BoneHorror <kilometrkm@gmail.com>
2025-02-21 22:33:15 +0100

    Some task info and more config in const

--- const.py
+++ const.py
@@ -7,7 +7,20 @@ class ANSI_COLORS:
     YELLOW = "\033[33m"
     BLUE = "\033[34m"
     CYAN = "\033[36m"
-
-P_CORES = range(0,12) #P/E denomer is used interchangeably with CCDs, where P is CCD0 and E is CCD1
-E_CORES = range(12,24)
+    
+#Whether this CPU supports SMT
 SMT = True
+
+#P/E denomer is used interchangeably with CCDs, where P is CCD0 and E is CCD1
+#Ryzen 9 7900 var.
+P_CORES = range(0,12) 
+E_CORES = range(12,24)
+
+#Ryzen 5 8500G var.
+#P_CORES = [0,1,8,9] 
+#E_CORES = [2,3,4,5,6,7,10,11]
+
+#Ryzen AI 9 365 var.
+#P_CORES = range(0,8)
+E_CORES = range(8,20)
+
--- tasks/tasks.py
+++ tasks/tasks.py
@@ -7,10 +7,11 @@ class SingleThreadTask:
         self.types_multicore_dict = {"None": False, "Integer Arithmetic": None, "Logical Operations": None, 
                                      "Memory Operations": None, "Complex Arithmetic": None, "Sorting Algorithm": None, 
                                      "Convolution Operations": None} 
-        self.types_used = []
+        self.last_task_infobits = []
 
     def matrix_multi(self, size = 2250):
         # Multiply two large matrices
+        self.last_task_infobits = ["FPU", "Memory", "SIMD Accel"]
         matrix_a = np.random.rand(size, size)
         matrix_b = np.random.rand(size, size)
         result = np.dot(matrix_a, matrix_b)
@@ -18,12 +19,14 @@ class SingleThreadTask:
 
     def sort_array(self, size = 22_500_000):
         # Sort a large array of random numbers
+        self.last_task_infobits = ["Integer", "SIMD Accel"]
         array = np.random.rand(size)
         sorted_array = np.sort(array)
         return sorted_array
 
     def calculate_prime_numbers(self, limit = 1_500_000):
         # Find all prime numbers up to limit
+        self.last_task_infobits = ["Integer", "Cond Branching"]
         primes = []
         for num in range(2, limit):
             is_prime = True
@@ -37,12 +40,14 @@ class SingleThreadTask:
 
     def fast_fourier_transform(self, size = 2_250_000):
         # Perform FFT on a large array
+        self.last_task_infobits = ["Complex arithmetic", "Memory"]
         array = np.random.rand(size)
         fft_result = np.fft.fft(array)
         return fft_result
     
     def image_processing(self, size = (5000, 5000)):
         # Apply Gaussian blur to a large image
+        self.last_task_infobits = ["FPU", "Memory", "SIMD Accel"]
         image = np.random.rand(*size)
         blurred_image = gaussian_filter(image, sigma=5)
         return blurred_image
